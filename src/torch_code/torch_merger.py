1f1d3923801a9972b4ac5593c482ba4d7b7e4ff0
Fabian von der Warth <fabianvonderwarth@gmail.com>
2024-10-17 10:32:36 +0200

    add script to merge

---
+++ src/torch_code/torch_merger.py
@@ -0,0 +1,7 @@
+import torch_in_memory_loader as torch_loader
+import os
+import sys
+
+if __name__ == "__main__":
+    loader = torch_loader.MCFDatasetInMemory(os.path.join(sys.argv[1], "train"))
+    loader2 = torch_loader.MCFDatasetInMemory(os.path.join(sys.argv[1], "test"))
