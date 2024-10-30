1fa625713bba5ab3bddd9c7a803588ef231ebe0b
Fabian von der Warth <fabianvonderwarth@gmail.com>
2024-10-30 10:39:23 +0100

    Refactor

--- src/trad_ml/compare_generated_instances.py
+++ src/trad_ml/compare_generated_instances.py
@@ -1,82 +1,32 @@
 # -*- coding: utf-8 -*-
 
-
 import pandas as pd
-import os
 import matplotlib.pyplot as plt
 import numpy as np
-from src.gen_data import util
+from src.gen_data import util as g_util
+from util import ALGO_NAMES
+from data_loader_trad import get_data
 import matplotlib as mpl
 import seaborn as sns
 
+plt.style.use("seaborn-v0_8")
+sns.set_palette("pastel")
 
+nice_fonts = {
+    # Use LaTeX to write all text
+    "font.family": "serif",
+    # Use 10pt font in plots, to match 10pt font in document
+    "axes.labelsize": 11,
+    "font.size": 11,
+    # Make the legend/label fonts a little smaller
+    "legend.fontsize": 12,
+    "xtick.labelsize": 10,
+    "ytick.labelsize": 10,
+}
+COLORS = dict(zip(ALGO_NAMES, ["red", "green", "blue", "yellow", "black", "orange", "purple"]))
 
-
-
-
-
-
-
-combined_data = get_data()
-
-"""
-combined_data_infeasible = combined_data[combined_data["Network Simplex"] == "There"]
-combined_data_feasible = combined_data[combined_data["Network Simplex"] != "There"]
-
-fig = plt.figure()
-ax = plt.axes()
-
-ax.scatter(combined_data_infeasible["g_supply"], combined_data_infeasible["g_mean_cap"])
-ax.set_xlabel("g_supply")
-ax.set_ylabel("g_mean_cap")
-ax.set_title("infeasible")
-
-fig = plt.figure()
-ax = plt.axes()
-
-ax.scatter(combined_data_feasible["g_supply"], combined_data_feasible["g_sum_cap"])
-ax.set_xlabel("g_supply")
-ax.set_ylabel("g_sum_cap")
-ax.set_title("feasible")
-
-length_before = len(combined_data_infeasible)
-
-to_delete = []
-for data, i in zip(combined_data_infeasible.iloc(), range(length_before)):
-    max_cap = data["g_max_cap"]
-    supply = data["g_supply"]
-    length = (data["g_number_of_nodes"] - 2) / (data["g_arcs_source_sink"] - 1)
-    mean_cap = data["g_mean_cap"]
-    index = int(combined_data_infeasible.index[i])
-    if mean_cap >= 0.95:
-        to_delete.append(index)
-    if supply > length * max_cap:
-        to_delete.append(index)
-    if 900 <= max_cap and max_cap <= 1000 and supply > 23000:
-        to_delete.append(index)
-
-    if 90 <= max_cap and max_cap <= 100 and supply > 2500:
-        to_delete.append(index)
-
-    if 9 <= max_cap and max_cap <= 10 and supply > 350:
-        to_delete.append(index)
-
-    if max_cap <= 5 and supply > 15:
-        to_delete.append(index)
-
-x = combined_data_infeasible.drop(to_delete, axis="index")
-length_after = len(x)
-
-print(length_before, length_after)
-
-fig = plt.figure()
-ax = plt.axes()
-ax.scatter(x["g_supply"], x["g_sum_cap"])
-ax.set_xlabel("g_supply")
-ax.set_ylabel("g_sum_cap")
-ax.set_title("infeasible")
-
-"""
+mpl.rcParams.update(nice_fonts)
+mpl.rcParams["figure.dpi"] = 1200
 
 
 def get_algorithm_from_seed(seed):
@@ -91,68 +41,28 @@ def get_algorithm_from_seed(seed):
         return "GO_TO"
 
 
-combined_data["Generator"] = combined_data.apply(get_algorithm_from_seed, axis=1)
-
-instances = len(combined_data)
-
-# calculate minimum of each instance
-
-
-colour = dict(zip(names_output, ["red", "green", "blue", "yellow", "black", "orange", "purple"]))
-generators = ["GRIDGEN", "NETGEN", "GO_TO", "GRIDGRAPH"]
-
-plt.style.use("seaborn")
-sns.set_palette("pastel")
-
-nice_fonts = {
-    # Use LaTeX to write all text
-    "text.usetex": True,
-    "font.family": "serif",
-    # Use 10pt font in plots, to match 10pt font in document
-    "axes.labelsize": 11,
-    "font.size": 11,
-    # Make the legend/label fonts a little smaller
-    "legend.fontsize": 12,
-    "xtick.labelsize": 10,
-    "ytick.labelsize": 10,
-}
-
-mpl.rcParams.update(nice_fonts)
-mpl.rcParams["figure.dpi"] = 1200
-
-
 def plot_features(gen):
     data = combined_data[combined_data["Generator"] == gen]
     for param in names_features[1:]:
         plt.figure(dpi=1200)
-        for alg in names_output[0:4]:
+        for alg in ALGO_NAMES[0:4]:
             unique_values = sorted(data[param].unique())
             y = [data[alg][data[param] == u].mean() for u in unique_values]
             p = np.polyfit(unique_values, y, 10)
             y = np.polyval(p, unique_values)
-            plt.plot(unique_values, y, c=colour[alg], label=alg)
+            plt.plot(unique_values, y, c=COLORS[alg], label=alg)
         plt.yscale("log")
         plt.legend()
         plt.title("param: " + param + ", generator: " + gen)
 
 
-def plot_alg():
-    number_of_minimum = np.array([len(combined_data[combined_data["Minimum"] == alg]) for alg in names_output])
-    indices_where_not_0 = [i for i in range(len(number_of_minimum)) if number_of_minimum[i] != 0]
-
-    names_output_modified = [names_output[i] for i in range(len(names_output)) if i in indices_where_not_0]
-
+def plot_fastest_algo_allgen():
+    number_of_minimum = np.array([len(combined_data[combined_data["Minimum"] == alg]) for alg in ALGO_NAMES])
     patches, _ = plt.pie(
-        number_of_minimum[indices_where_not_0], colors=sns.color_palette("Set2", 7), labels=["NS", "CS2", "SSP", "CAS", "", "", ""]
-    )  # wedgeprops = {'linewidth': 1, "edgecolor":"k"},
-    # plt.legend(patches, names_output_modified, loc = "best",bbox_to_anchor=(0, 1.))
-
-    # plt.title("Fastest algorithms on all instances (n=" + str(instances) + ")")
-
-
-colour = dict(zip(names_output, ["red", "green", "blue", "yellow", "black", "orange", "purple"]))
-plot_alg()
-
+        number_of_minimum, colors=sns.color_palette("Set2", 7),
+        labels=ALGO_NAMES
+    )
+    plt.savefig("pieplot_algorithms_all.png")
 
 def plot_alg_generators():
     datanet = combined_data[combined_data["Generator"] == "NETGEN"]
@@ -161,65 +71,69 @@ def plot_alg_generators():
     datagoto = combined_data[combined_data["Generator"] == "GO_TO"]
 
     # Netgen
-    number_of_minimum_net = np.array([len(datanet[datanet["Minimum"] == alg]) for alg in names_output])
+    number_of_minimum_net = np.array([len(datanet[datanet["Minimum"] == alg]) for alg in ALGO_NAMES])
     indices_where_not_0 = [i for i in range(len(number_of_minimum_net)) if number_of_minimum_net[i] != 0]
-
-    names_output_modified = [names_output[i] for i in range(len(names_output)) if i in indices_where_not_0]
 
     fig, axs = plt.subplots(2, 3, figsize=(7, 5))
     patches, _ = axs[0, 0].pie(number_of_minimum_net[indices_where_not_0], colors=sns.color_palette("Set2", 7))
-
-    netgeninstances = len(datanet)
     axs[0, 0].set_title("Netgen")
 
     # Gridgen
-    number_of_minimum_ggen = np.array([len(dataggen[dataggen["Minimum"] == alg]) for alg in names_output])
+    number_of_minimum_ggen = np.array([len(dataggen[dataggen["Minimum"] == alg]) for alg in ALGO_NAMES])
     indices_where_not_0 = [i for i in range(len(number_of_minimum_ggen)) if number_of_minimum_ggen[i] != 0]
 
-    names_output_modified = [names_output[i] for i in range(len(names_output)) if i in indices_where_not_0]
-
     patches, _ = axs[0, 1].pie(number_of_minimum_ggen[indices_where_not_0], colors=sns.color_palette("Set2", 7))
-
-    gridgeninstances = len(dataggen)
     axs[0, 1].set_title("Gridgen")
 
     # Gridgraph
-    number_of_minimum_ggraph = np.array([len(dataggraph[dataggraph["Minimum"] == alg]) for alg in names_output])
+    number_of_minimum_ggraph = np.array([len(dataggraph[dataggraph["Minimum"] == alg]) for alg in ALGO_NAMES])
     indices_where_not_0 = [i for i in range(len(number_of_minimum_ggraph)) if number_of_minimum_ggraph[i] != 0]
-
-    names_output_modified = [names_output[i] for i in range(len(names_output)) if i in indices_where_not_0]
 
     patches, _ = axs[1, 0].pie(number_of_minimum_ggraph[indices_where_not_0], colors=sns.color_palette("Set2", 7))
     fig.legend(patches[:-3], ["NS", "CS2", "SSP", "CAS"], loc="upper right")
 
-    ggraphinstances = len(dataggraph)
     axs[1, 0].set_title("Gridgraph")
 
     # Goto
-    number_of_minimum_goto = np.array([len(datagoto[datagoto["Minimum"] == alg]) for alg in names_output])
+    number_of_minimum_goto = np.array([len(datagoto[datagoto["Minimum"] == alg]) for alg in ALGO_NAMES])
     indices_where_not_0 = [i for i in range(len(number_of_minimum_ggraph)) if number_of_minimum_goto[i] != 0]
 
-    names_output_modified = [names_output[i] for i in range(len(names_output)) if i in indices_where_not_0]
-
     patches, _ = axs[1, 1].pie(number_of_minimum_goto[indices_where_not_0], colors=sns.color_palette("Set2", 7))
-
-    gotoinstances = len(datagoto)
     axs[1, 1].set_title("Goto")
 
     axs[0, 2].axis("off")
 
     # Alle
-    number_of_minimum = np.array([len(combined_data[combined_data["Minimum"] == alg]) for alg in names_output])
+    number_of_minimum = np.array([len(combined_data[combined_data["Minimum"] == alg]) for alg in ALGO_NAMES])
     indices_where_not_0 = [i for i in range(len(number_of_minimum)) if number_of_minimum[i] != 0]
-
-    names_output_modified = [names_output[i] for i in range(len(names_output)) if i in indices_where_not_0]
 
     patches, _ = axs[1, 2].pie(number_of_minimum[indices_where_not_0], colors=sns.color_palette("Set2", 7))
 
     axs[1, 2].set_title("All")
+    #TODO: Legend, make order of colors the same if possible.
+
+    plt.savefig("test.png")
+
+if __name__ == "__main__":
+    combined_data_train = get_data(True)
+    combined_data_test = get_data(False)
+    combined_data = pd.concat([combined_data_train, combined_data_test])
+    combined_data["Generator"] = combined_data.apply(get_algorithm_from_seed, axis=1)
+
+    instances = len(combined_data)
+    #plot_fastest_algo_allgen()
+    plot_alg_generators()
+
+"""
 
 
-plot_alg_generators()
+
+
+
+
+
+
+# plot_alg_generators()
 
 
 # generator = "GRIDGEN"
@@ -242,7 +156,8 @@ def plot_results(alle=True):
         classifiers,
         accuracies,
         color=[
-            (0.4, 0.7607843137254902, 0.6470588235294118) if clf != "NS" else (0.9882352941176471, 0.5529411764705883, 0.3843137254901961)
+            (0.4, 0.7607843137254902, 0.6470588235294118) if clf != "NS" else (
+            0.9882352941176471, 0.5529411764705883, 0.3843137254901961)
             for clf in classifiers
         ],
     )
@@ -255,10 +170,10 @@ def plot_results(alle=True):
 
     if alle:
         # classifiers.remove("Gauss")
-        time = [x["test_time"] / 10**6 for x in results if x["name"] != "GaussianNB"]
+        time = [x["test_time"] / 10 ** 6 for x in results if x["name"] != "GaussianNB"]
     else:
         classifiers.remove("GaussP")
-        time = [x["test_time"] / 10**6 for x in results if x["name"] != "GaussianProcessClassifier"]
+        time = [x["test_time"] / 10 ** 6 for x in results if x["name"] != "GaussianProcessClassifier"]
 
     plt.figure(dpi=1200)
     plt.bar(
@@ -284,7 +199,7 @@ def plot_results(alle=True):
 
     classifiers = ["DTree", "RForest", "NetworkSimplex", "GroundTruth"]
     time = [
-        x["test_time"] / 10**6
+        x["test_time"] / 10 ** 6
         for x in results
         if x["name"] in ["DecisionTreeClassifier", "RandomForestClassifier", "baseline", "GroundTruth"]
     ]
@@ -317,14 +232,15 @@ def plot_results(alle=True):
     def autolabel(rects):
         for idx, rect in enumerate(barplot):
             height = rect.get_height()
-            plt.text(rect.get_x() + rect.get_width() / 2.0, height, bar_label[idx], ha="center", va="bottom", rotation=0)
+            plt.text(rect.get_x() + rect.get_width() / 2.0, height, bar_label[idx], ha="center", va="bottom",
+                     rotation=0)
 
     autolabel(barplot)
 
 
-plot_results(True)
+# plot_results(True)
 
-"""
+
 def plot_features():
     for param in names_features[1:]:
         plt.figure()
@@ -337,7 +253,7 @@ def plot_features():
         plt.legend()
         plt.title(param)
 
-
+"""
 
 # plot_features("GRIDGRAPH")
 
@@ -364,4 +280,3 @@ def plot_features():
 #     plt.yscale("log")
 #     plt.legend()
 #     plt.title(param)
-"""
